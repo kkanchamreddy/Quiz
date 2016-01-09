6f21548dfc1cf3495de2a3078a988cf2dd5d1951
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-08 22:25:31 -0800

    Handle screen rotation in CheatActivity

--- app/src/main/java/com/kkiran/quiz/activity/CheatActivity.java
+++ app/src/main/java/com/kkiran/quiz/activity/CheatActivity.java
@@ -22,6 +22,16 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private boolean mIsAnswerShown;
+
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_ANSWER_SHOWN = "isAnswerShown";
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_ANSWER_SHOWN,mIsAnswerShown);
+    }
 
     public static Intent newIntent(Context packageContext, boolean isAnswerTrue) {
         Intent i = new Intent(packageContext, CheatActivity.class);
@@ -37,6 +47,11 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+
+        if(savedInstanceState != null) {
+            mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
+            setAnswerShownResult(mIsAnswerShown);
+        }
 
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +92,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
+        mIsAnswerShown = isAnswerShown;
         data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown );
         setResult(RESULT_OK, data);
     }
